626779a700f735dc0917ea1fee2cce833ea196c1
Siddharth Gupta <siddharthgup3@gmail.com>
2020-04-26 18:07:36 +0530

    Added Gun Definition SO
    Initial Gun Def SO

---
+++ GunSystem/Assets/Scripts/EquippedGun.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedGun : MonoBehaviour
+{
+    public GunDefinition currentGun;
+
+
+}
---
+++ GunSystem/Assets/Scripts/GunDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New GunDefinition", menuName = "GunTemplate")]
+public class GunDefinition : ScriptableObject
+{
+    public string gunName;
+    public bool isAuto;
+    public bool isSemi;
+    public int maxAmmo;
+    
+    
+    public GameObject gunModel;
+}
