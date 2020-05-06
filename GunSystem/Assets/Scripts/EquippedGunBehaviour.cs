cbeba8a581473f307325ca1531ba88cc9fdc2c68
Siddharth Gupta <siddharthgup3@gmail.com>
2020-05-06 19:51:16 +0530

    Using enums, Updated Architecture
    Next:
    Finally start working on Raycasts, and recoil system

--- GunSystem/Assets/Scripts/EquippedGunBehaviour.cs
+++ GunSystem/Assets/Scripts/EquippedGunBehaviour.cs
@@ -12,13 +12,19 @@ public class EquippedGunBehaviour : MonoBehaviour
     public AllGunStatus inventory;
 
     public bool isReloading = false;
-    public float timeLeft = 0f;
+    public bool isScoping = false;
+
+
     private bool readyToFire = true;
     private bool usingADS = false;
     private bool usingBurst = false;
 
     private float rateOfFire = 0;
     private float fireTimer = 0;
+
+
+    private float burstTimer;
+    private int burstCounter;
 
     private bool ammoLeft = true;
     void Start()
@@ -33,64 +39,108 @@ public class EquippedGunBehaviour : MonoBehaviour
         else
             ammoLeft = true;
 
+
         CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            StartReload();
+        CheckModeChange();
+        CheckShoot();
     }
 
     private void FixedUpdate()
     {
-        fireTimer += Time.deltaTime;
+        fireTimer += Time.fixedDeltaTime;
+
+
+        if(usingBurst)
+        {
+            burstTimer += Time.fixedDeltaTime;
+            if (burstTimer >= currentGun.burstPause)
+            {
+                burstCounter = 0;
+                burstTimer = 0.0f;
+            }
+        }
     }
 
-
-    public void CheckInput()
+    public void CheckModeChange()
     {
-        
         if (Input.GetMouseButtonDown(1))
         {
-            if (currentGun.isBurst)
-                usingBurst = !usingBurst;
-            else if (currentGun.isADS)
+            if (currentGun.shootType == ShootType.ADS)
+            {
                 usingADS = !usingADS;
+                isScoping = true;
+                Invoke("ModifyScope", currentGun.scopeInTime);
+            }
         }
 
-        if (currentGun.isSemi)
+        if (Input.GetMouseButtonDown(2))
         {
-            if (Input.GetMouseButtonDown(0) && readyToFire && fireTimer >= rateOfFire) // Merge these conditions in Update later.
-            {
-                //Fire once
-                if (usingADS && ammoLeft)
-                    FireADS();
-                else if (ammoLeft)
-                    Fire();
-                else
-                    DryFire();
-                readyToFire = false;
-            }
-
-            if (Input.GetMouseButtonUp(0))
-                readyToFire = true;
-        }
-
-        else if (currentGun.isAuto)
-        {
-            if (Input.GetMouseButton(0) && fireTimer >= rateOfFire)
-            {
-                //keep firing
-                if (usingADS && ammoLeft)
-                    FireADS();
-                else if (ammoLeft)
-                    Fire();
-                else
-                    DryFire();
-            }
+            if (currentGun.burstOnly)
+                return;
+            else if (currentGun.burstType == BurstType.isBurst)
+                usingBurst = !usingBurst;
         }
     }
 
+    public void CheckShoot()
+    {
+        //Should not be reloading, not be scoping in, should have ammo, should be greater than burst pause..should be greater than fire rate 
+        // andd...readytofire(in case of smg)
+
+        if (currentGun.gunType == GunType.Full)
+        {
+            if(Input.GetMouseButton(0) && fireTimer >= rateOfFire && !isReloading && !isScoping)
+            {
+                readyToFire = false;
+
+                if (!ammoLeft)
+                    DryFire();
+                else if (burstCounter <= currentGun.burstRate)
+                {
+                    if (usingADS)
+                        FireADS();
+                    else
+                        Fire();
+                }
+            }
+        }
+
+        else if(currentGun.gunType == GunType.Semi)
+        {
+            if (Input.GetMouseButtonUp(0))
+                readyToFire = true;
+
+            if (Input.GetMouseButton(0) && fireTimer >= rateOfFire && readyToFire && !isReloading && !isScoping)
+            {
+                if (!ammoLeft)
+                    DryFire();
+                else if (burstCounter <= currentGun.burstRate)
+                {
+                    if (usingADS)
+                        FireADS();
+                    else
+                        Fire();
+                }
+            }
+        }
+    }
 
     public void OnChange(GunDefinition newGunDef)
     {
         currentGun = newGunDef;
         usingADS = false;
+
+        if (currentGun.burstOnly)
+            usingBurst = true;
+        else
+            usingBurst = false;
+
         rateOfFire = 1 / currentGun.firingRate;
         fireTimer = rateOfFire;
         ammoLeft = true;
@@ -100,6 +150,9 @@ public class EquippedGunBehaviour : MonoBehaviour
     public void Fire()
     {
         fireTimer = 0f;
+        if (usingBurst)
+            burstCounter++;
+
         //Fire stuff here
         Debug.Log("Firing");
         inventory.status[currentGun.name]--;
@@ -108,6 +161,9 @@ public class EquippedGunBehaviour : MonoBehaviour
     public void FireADS()
     {
         fireTimer = 0f;
+        if (usingBurst)
+            burstCounter++;
+
         //Firing from ADS here!
         Debug.Log("Firing");
         inventory.status[currentGun.name]--;
@@ -116,14 +172,26 @@ public class EquippedGunBehaviour : MonoBehaviour
     public void DryFire()
     {
         //Do Something and the call reload
-        Invoke("Reload", currentGun.reloadTime);
-        isReloading = true;   
+        StartReload();
     }
 
+    public void ModifyScope()
+    {
+        isScoping = false;
+    }
+    public void StartReload()
+    {
+        if (inventory.status[currentGun.name] == currentGun.maxAmmo)
+            return;
+
+        isReloading = true;
+        Invoke("Reload", currentGun.reloadTime);
+    }
+    
     public void Reload()
     {
         Debug.Log("Starting Reload");
-        if(isReloading)
+        if (isReloading)
         {
             isReloading = false;
             inventory.status[currentGun.name] = currentGun.maxAmmo;
--- GunSystem/Assets/Scripts/GunDefinition.cs
+++ GunSystem/Assets/Scripts/GunDefinition.cs
@@ -2,6 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GunType
+{
+    Full,
+    Semi
+}
+
+public enum BurstType
+{
+    isBurst,
+    noBurst
+}
+
+public enum ShootType
+{
+    ADS,
+    Hipfire
+}
+
+public enum ShootMode
+{
+    Normal,
+    SingleShot
+}
 
 [CreateAssetMenu(fileName = "New GunDefinition", menuName = "GunTemplate")]
 public class GunDefinition : ScriptableObject
@@ -9,23 +32,26 @@ public class GunDefinition : ScriptableObject
     public string gunName;
     public Mesh model;
 
+    public GunType gunType;
+    public BurstType burstType;
+    public ShootType shootType;
+    public ShootMode shootMode;
+
     public int shotsPerRound;
     public float firingRate;
 
     public int maxAmmo;
     public int maxClips;
 
-    public bool isAuto;
-    public bool isSemi;
 
-    public bool isADS;
-    public float scopeInTime;
+    public bool burstOnly;
 
 
-    public bool isBurst;
-    public int burstPause;
-    public int burstCounter;
-    public int burstRate;
+    public float scopeInTime;     //Require this only if isADS is true. 
+
+ 
+    public int burstPause;       //Require this only if isBurst is true. 
+    public int burstRate;        //Require this only if isBurst is true. 
 
     public int reloadTime;
 
