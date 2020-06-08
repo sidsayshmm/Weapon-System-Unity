8d55afdb85cb92218203ecbd974b21c21339e70d
Siddharth Gupta <siddharthgup3@gmail.com>
2020-06-08 19:47:11 +0530

    pages test

--- GunSystem/Assets/Scripts/EquippedGunBehaviour.cs
+++ GunSystem/Assets/Scripts/EquippedGunBehaviour.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class EquippedGunBehaviour : MonoBehaviour
