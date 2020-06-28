090631663d3ce0b94794bd344a47aeb0de9872b9
Siddharth Gupta <siddharthgup3@gmail.com>
2020-06-28 17:15:11 +0530

    Updated race conditions

--- GunSystem/Assets/Scripts/Gun Scripts/EquippedGun.cs
+++ GunSystem/Assets/Scripts/Gun Scripts/EquippedGun.cs
@@ -31,14 +31,27 @@ public class EquippedGun : MonoBehaviour
 
     [ReadOnly] public bool doingAction = false;
     private bool isReloading = false;
+    private bool isScoping = false;
 
     public float actionTime = 0f;
     public float sliderMax = 1f;
+
+    private float startScopeTime;
     #endregion
 
     private Coroutine reloadCoroutine;
     private Coroutine scopeCoroutine;
     [ReadOnly] [Range(10, 250)] public float currentAcc;
+
+
+    #region Properties
+    
+    public float CurrentAccuracy
+    {
+        get { return currentAcc; }
+    }
+
+    #endregion
 
     private void Awake()
     {
@@ -105,7 +118,15 @@ public class EquippedGun : MonoBehaviour
             if(Input.GetMouseButton(1) && !doingAction)
             {
                 Debug.Log("Starting scope");
+                startScopeTime = Time.time;
                 scopeCoroutine = StartCoroutine(Scoping(currentGun.scopeInTime));
+            }
+            else if(Input.GetMouseButtonDown(1) && isScoping)
+            {
+                float scopeOutTime = Time.time - startScopeTime;
+                if (scopeCoroutine != null)
+                    StopCoroutine(scopeCoroutine);
+                scopeCoroutine = StartCoroutine(Scoping(scopeOutTime));
             }
         }
     }
@@ -159,7 +180,6 @@ public class EquippedGun : MonoBehaviour
         SetSlider(rateOfFire);
         actionTime = rateOfFire;
     }
-
     public IEnumerator Reload()
     {
         SetSlider(currentGun.reloadTime);
@@ -174,18 +194,21 @@ public class EquippedGun : MonoBehaviour
         doingAction = false;
         inventory.status[currentGun.name] = currentGun.clipSize;
     }
-
     public IEnumerator GunChange(BaseGunDefinition newGunDef, float timer)
     {
         if(isReloading)
-        {
             StopCoroutine(reloadCoroutine);
-            doingAction = false;
-            isReloading = false;
-            actionTime = 0;
-        }
+
+        if(isScoping)
+            StopCoroutine(scopeCoroutine);
+
+        isReloading = false;
+        actionTime = 0;
+
         doingAction = true;
         actionTime = timer;
+        SetSlider(timer);
+
         yield return new WaitForSeconds(timer);
         UpdateGun(newGunDef);
         doingAction = false;
@@ -194,17 +217,22 @@ public class EquippedGun : MonoBehaviour
     public IEnumerator Scoping(float timer)
     {
         Debug.Log($"Starting scoping at {Time.time}");
+        Debug.Log(timer);
         doingAction = true;
+        isScoping = true;
         actionTime = timer;
+        SetSlider(timer);
         yield return new WaitForSeconds(timer);
+
         doingAction = false;
         if (currentSightMode == SightType.ADS)
             currentSightMode = SightType.Normal;
         else
             currentSightMode = SightType.ADS;
 
+        isScoping = false;
+        doingAction = false;
         Debug.Log($"Current shoot mode = { currentSightMode} at {Time.time} ");
-        
     }
 
 
